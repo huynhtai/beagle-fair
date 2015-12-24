15621f1cae0b023c3eaee7bc11278eb97f733792
Tai Phi Thien Huynh <tai.huynh@smartdev.vn>
2015-12-24 16:51:42 +0700

    add file ErrorHandlingException.java

---
+++ beagle-fair-webservice/src/main/java/ch/smartlinksa/intern/webservice/controller/ErrorHandlingController.java
@@ -0,0 +1,65 @@
+package ch.smartlinksa.intern.webservice.controller;
+
+
+import ch.smartlinksa.intern.interfaces.response.RestApiResponse;
+import ch.smartlinksa.intern.interfaces.response.RestApiResponseHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@ControllerAdvice
+public class ErrorHandlingController {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public RestApiResponse<?> handleMethodArgumentNotValidExceptionS(HttpServletRequest request, HttpServletResponse response,
+                                                     Object handler, MethodArgumentNotValidException ex) {
+        BindingResult result = ex.getBindingResult();
+
+        if (isObjectValidateError(result)) {
+            ObjectError error = result.getGlobalError();
+            return createPaymentResponseError(request, error.getDefaultMessage(), null);
+        } else {
+            FieldError fieldError = result.getFieldErrors().get(0);
+            return createPaymentResponseError(request, fieldError.getDefaultMessage(), fieldError.getArguments());
+        }
+
+    }
+
+    private RestApiResponse<?> createPaymentResponseError(HttpServletRequest request, String resultCode, Object[] messageArguments) {
+        RestApiResponse<String> RestApiResponse = new RestApiResponse<String>();
+        RestApiResponseHeaders responseHeaders = RestApiResponse.getHeaders();
+        responseHeaders.setClientTransactionId("");
+        responseHeaders.setTransactionId("");
+        responseHeaders.setResultCode(resultCode);
+        responseHeaders.setResultDescription(getMessage(resultCode, messageArguments));
+        return RestApiResponse;
+    }
+
+
+    private boolean isObjectValidateError(BindingResult result) {
+        return result.getGlobalErrorCount() > 0;
+    }
+
+    private String getMessage(String resultCode, Object[] messageAgruments) {
+        try {
+            return messageSource.getMessage(resultCode, messageAgruments, LocaleContextHolder.getLocale());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resultCode;
+    }
+}
