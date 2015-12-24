84967dac81b98bb2e10ce001325f3c61dcf84579
PhoPhaoHoa <mailien.bk@gmail.com>
2015-12-24 15:55:12 +0700

    config validation

--- beagle-fair-webservice/src/main/java/ch/smartlinksa/intern/webservice/controller/UserController.java
+++ beagle-fair-webservice/src/main/java/ch/smartlinksa/intern/webservice/controller/UserController.java
@@ -8,13 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 public class UserController {
     @Autowired
     IUserService userService;
 
     @RequestMapping(value = "/adduser", method = RequestMethod.POST)
-    public String add(@RequestBody UserRequest userRequest){
+    public String add(@RequestBody @Valid UserRequest userRequest){
         System.out.println("UserRequest in Controller: " + userRequest.getFirstName());
         return userService.add(userRequest);
     }
