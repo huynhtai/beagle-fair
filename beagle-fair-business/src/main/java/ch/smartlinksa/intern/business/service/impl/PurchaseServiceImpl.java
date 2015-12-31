b95e2bbe33c4be615483eba4d4b4f643668a75e4
huynhtai <taihuynhbk@gmail.com>
2015-12-31 11:59:48 +0700

    check balance when user not have enough money to purchase, add UserNotHaveEnoughToPurchaseException

--- beagle-fair-business/src/main/java/ch/smartlinksa/intern/business/service/impl/PurchaseServiceImpl.java
+++ beagle-fair-business/src/main/java/ch/smartlinksa/intern/business/service/impl/PurchaseServiceImpl.java
@@ -1,9 +1,12 @@
 package ch.smartlinksa.intern.business.service.impl;
 
+import ch.smartlinksa.intern.business.exception.UserNotEnoughMoneyToPurchaseException;
 import ch.smartlinksa.intern.business.service.IPurchaseService;
 import ch.smartlinksa.intern.business.util.SessionUtil;
 import ch.smartlinksa.intern.dao.entity.PurchaseTransaction;
+import ch.smartlinksa.intern.dao.entity.User;
 import ch.smartlinksa.intern.dao.repository.PurchaseTransactionRepository;
+import ch.smartlinksa.intern.dao.repository.UserRepository;
 import ch.smartlinksa.intern.interfaces.constant.MessageCodeConstant;
 import ch.smartlinksa.intern.interfaces.request.PurchaseRequest;
 import ch.smartlinksa.intern.interfaces.response.PurchaseResponse;
@@ -17,9 +20,17 @@ public class PurchaseServiceImpl implements IPurchaseService {
     @Autowired
     PurchaseTransactionRepository purchaseTransactionRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
     public RestApiResponse<PurchaseResponse> addNewPurchase(PurchaseRequest purchaseRequest) {
         PurchaseTransaction purchaseTransaction = convertPurchaseRequestToPurchaseEntity(purchaseRequest);
+        User user = SessionUtil.getCurrentUser();
+        if(isNotEnoughMoneyToPurchase(purchaseTransaction, user)){
+            throw new UserNotEnoughMoneyToPurchaseException();
+        }
         purchaseTransactionRepository.save(purchaseTransaction);
+        updateBalanceOfUser(purchaseTransaction, user);
         return makePurchaseTransactionResponse(convertPurchaseEntityToPurchaseTransactionResponse(purchaseTransaction));
     }
 
@@ -55,6 +66,19 @@ public class PurchaseServiceImpl implements IPurchaseService {
 
     private double calculateTotalPrice(PurchaseRequest purchaseRequest) {
         return purchaseRequest.getQuantity() * purchaseRequest.getUnitPrice();
+    }
+
+    private boolean isNotEnoughMoneyToPurchase(PurchaseTransaction purchaseTransaction, User user){
+        boolean isNotEnoughMoney = false;
+        if(purchaseTransaction.getTotalPrice() > user.getBalance()){
+            isNotEnoughMoney = true;
+        }
+        return isNotEnoughMoney;
+    }
+    private void updateBalanceOfUser(PurchaseTransaction purchaseTransaction, User user){
+        double balanceAfterPurchase = user.getBalance() - purchaseTransaction.getTotalPrice();
+        user.setBalance(balanceAfterPurchase);
+        userRepository.save(user);
     }
 
 }
